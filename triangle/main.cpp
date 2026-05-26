93745680d54a86527ae78ebc4acecd8fa52add30
jaydhurve09 <jaydhurve09@gmail.com>
2026-05-26 12:09:10 +0530

    Surface and Renderer created and used in SDL window

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -1,17 +1,54 @@
-#include <iostream>
 #include <SDL3/SDL.h>
+#include <iostream>
 
 int main() {
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_Log("SDL_Init failed: %s", SDL_GetError());
-		return -1;
-	}
+    SDL_Window* window;
+    SDL_Surface* surface;
+    SDL_Renderer* renderer;
+	SDL_Texture* texture;
 
+    if (SDL_Init(SDL_INIT_VIDEO) == false) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return -1;
+    }
 
-	// Create a window with the title "Triangle", dimensions 320x240, and make it resizable.
-	SDL_Window* window = SDL_CreateWindow("Triangle", 320, 240, SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow(
+        "Triangle",
+        320,
+        240,
+        SDL_WINDOW_RESIZABLE
+    );
 
-	SDL_Delay(10000);
+    if (!window) {
+        SDL_Log("Window creation failed: %s", SDL_GetError());
+        return -1;
+    }
 
-	SDL_Quit();
+    renderer = SDL_CreateRenderer(window, NULL);
+
+    surface = SDL_LoadBMP("sample.bmp");
+
+    if (!surface) {
+        SDL_Log("BMP load failed: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Log("BMP loaded successfully!");
+
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderTexture(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroySurface(surface);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
 }
